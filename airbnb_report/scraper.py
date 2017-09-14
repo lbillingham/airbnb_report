db2c1c4d107f36570db388df9ba44d95864bebd0
Laurence Billingham <laurence.billingham@gmail.com>
2017-09-14 20:44:11 +0100

    passing listing info getting

--- airbnb_report/scraper.py
+++ airbnb_report/scraper.py
@@ -18,7 +18,21 @@ MAGIC_ATTR = 'data-reactid'
 divs = soup.find_all('div', attrs={MAGIC_ATTR: True, 'class': MAGIC_CLASS})
 ```
 but that is likely to be more brittle.
+
+## Deliberate compromises
+
+1. Getting the JSON is potentilly brittle: but then so is scraping in general.
+   There is probably an, undocumented, API that we might get the JSON from...
+   maybe that would be less likely to change
+
+2. I'm writing tests 1st this, an personal preference, leads to lots of small
+   functions. Each function owns its 'state' (e.g. the url template string).
+   This means lots of moving parts to mentally juggle, but _should_
+   be loosly coupled and therefore more flexible.
 """
+import warnings
+
+from bs4 import BeautifulSoup
 
 def airbnb_url_for(listing_number):
     return f'https://www.airbnb.co.uk/rooms/{listing_number}'
@@ -28,8 +42,17 @@ def script_tags(soup):
     tags = soup.find_all('script', attrs={'type': 'application/json'})
     return tags
 
-def listing_info_tag(_):
-    pass
+def listing_info_tag(tags):
+    infos = [t for t in tags if 'listingInfo' in t.text]
+    if len(infos) > 1:
+        mess = 'multiple script tags with "listingInfo" data found: using 1st one found'
+        warnings.warn(mess)
+    try:
+        info = infos[0]
+    except IndexError:
+        mess = 'no script tags with "listingInfo" data found, did the Airbnb API change?'
+        raise ValueError(mess)
+    return info
 
 def scrape(url):
     return url
